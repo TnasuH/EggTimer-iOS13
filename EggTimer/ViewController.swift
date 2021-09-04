1a895965f52a108ea979a58889d395e379e88142
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-09-04 22:19:11 +0300

    timer added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var labelUI: UILabel!
+    
+    let eggTimes = ["Soft":5,"Medium":420,"Hard":720]
+    
+    var secondsRemaining = 0
+    
+    var timer: Timer = Timer()
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        self.secondsRemaining = self.eggTimes[sender.currentTitle!]!
+        self.timer.invalidate()
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            print("\(self.secondsRemaining) seconds..")
+            self.secondsRemaining = self.secondsRemaining - 1
+            if self.secondsRemaining < 0 {
+                self.labelUI.text = "Time Up!"
+                timer.invalidate()
+                
+            }
+        }
+    }
+    
+    
+    
+    
 }
