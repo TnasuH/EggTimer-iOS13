0f9aa129c03a7710af69bdd20506234ea19db39f
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-09-04 22:40:32 +0300

    progressBar added and completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var labelUI: UILabel!
+    @IBOutlet weak var progressBarUI: UIProgressView!
     
-    let eggTimes = ["Soft":5,"Medium":420,"Hard":720]
+    let eggTimes = ["Soft":320,"Medium":420,"Hard":720]
     
     var secondsRemaining = 0
     
@@ -21,13 +22,16 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         self.secondsRemaining = self.eggTimes[sender.currentTitle!]!
         self.timer.invalidate()
+        
         self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            
+            self.progressBarUI.progress =
+                Float(1) - ( Float(self.secondsRemaining) / Float(self.eggTimes[sender.currentTitle!]!))
             print("\(self.secondsRemaining) seconds..")
             self.secondsRemaining = self.secondsRemaining - 1
             if self.secondsRemaining < 0 {
                 self.labelUI.text = "Time Up!"
                 timer.invalidate()
-                
             }
         }
     }
